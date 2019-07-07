7c6301257392451183ed0c81dff38c36796122fc
Tomasz Rusin <tomaszrusin@op.pl>
2019-07-07 23:02:56 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react';
 import './App.css';
+import PlayersList from './components/PlayersList/PlayersList'
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      players: [
+        {
+          name: 'Kundegunda',
+          score: 5,
+        },
+        {
+          name: 'Antoś',
+          score: 0,
+        }
+      ]
+    }
+  };
+
+  onScoreUpdate = (index, points) => {
+   
+        this.setState({
+          players: [
+            this.state.players.map((player, i) => {
+              if(i === index){
+                return ({
+                  name: player.name,
+                  score: player.score + points
+                })
+              }else {
+                return player
+              }
+            })
+          ]
+        })
+        // player.score = player.score + points
+      
+  };
+
   render() {
     return (
       <div className="App">
+        <PlayersList players={this.state.players} onScoreUpdate={this.onScoreUpdate}/>
       </div>
     );
   }
