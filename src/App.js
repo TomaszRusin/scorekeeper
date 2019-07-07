7c5d821b25f0b5cbdf80bada19a74cee764baecf
Tomasz Rusin <tomek@rusin.info>
2019-07-08 00:07:04 +0200

    Add addPlayer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import PlayersList from './components/PlayersList/PlayersList'
+import AddPlayer from './components/AddPlayer/AddPlayer'
 
 class App extends Component {
   constructor() {
@@ -10,7 +11,7 @@ class App extends Component {
       players: [
         {
           name: 'Kundegunda',
-          score: 5,
+          score: 0,
         },
         {
           name: 'Antoś',
@@ -20,29 +21,32 @@ class App extends Component {
     }
   };
 
-  onScoreUpdate = (index, points) => {
-   
-        this.setState({
-          players: [
-            this.state.players.map((player, i) => {
-              if(i === index){
-                return ({
-                  name: player.name,
-                  score: player.score + points
-                })
-              }else {
-                return player
-              }
-            })
-          ]
-        })
-        // player.score = player.score + points
-      
-  };
+onScoreUpdate = (playerIndex, scoreChange) => {
+  this.setState({
+    players: this.state.players.map((player, index) => {
+      if (index === playerIndex) {
+        return { ...player, score: player.score + scoreChange };
+      }
+      return player;
+    })
+  })
+}
+
+onPlayerAdd = (name) => {
+  const newPlayer = {
+    name: name,
+    score: 0,
+  }
+  
+  this.setState({
+    players: [...this.state.players, newPlayer]
+  })
+}
 
   render() {
     return (
       <div className="App">
+        <AddPlayer onPlayerAdd={this.onPlayerAdd}/>
         <PlayersList players={this.state.players} onScoreUpdate={this.onScoreUpdate}/>
       </div>
     );
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import PlayersList from './components/PlayersList/PlayersList'
 
 it('renders without crashing', () => {
   shallow(<App />);
 });
+it('should update player score', () => {
+  const component = shallow(<App />)
+
+  // const players = [
+  //   {
+  //       name: 'Kunegunda',
+  //       score: 0
+  //   }
+  // ];
+
+  // component.setState({players})
+  const onScoreUpdate = component.find(PlayersList).prop('onScoreUpdate');
+  onScoreUpdate(0, 5);
+  const playersAfterUpdate = component.state().players;
+
+  expect(playersAfterUpdate[0].score).toBe(5)
+})
